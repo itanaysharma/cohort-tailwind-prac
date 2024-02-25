8d5f7080713d003510910f84cb9c6731e4e5fb62
itanaysharma <thisistanaysharma@gmail.com>
2024-02-25 22:53:21 +0530

    giving different size in flex and grid

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ function App() {
         <div style={{ backgroundColor: "green" }}>hi</div>
         <div style={{ backgroundColor: "yellow" }}>hi</div>
       </div>
-      <div className="flex justify-around">
-        <div className="bg-red-500">hi</div>
-        <div className="bg-green-500">hi</div>
-        <div className="bg-yellow-500">hi</div>
+      <div className="flex ">
+        <div className="bg-red-500 w-[40%]">hi</div>
+        <div className="bg-green-500 w-[40%]">hi</div>
+        <div className="bg-yellow-500 w-[20%]">hi</div>
       </div>
-      <div className="grid grid-cols-3">
-        <div className="bg-red-500">hi</div>
-        <div className="bg-green-500">hi</div>
-        <div className="bg-yellow-500">hi</div>
+      <div className="grid grid-cols-10">
+        <div className="bg-red-500 col-span-4">hi</div>
+        <div className="bg-green-500 col-span-4">hi</div>
+        <div className="bg-yellow-500 col-span-2">hi</div>
       </div>
     </>
   );
