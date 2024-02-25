8e34fbc09e42dd0a2de452883cfcce3ff5378cb6
itanaysharma <thisistanaysharma@gmail.com>
2024-02-25 22:03:55 +0530

    tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div style={{ backgroundColor: "red" }}>hi</div>
+        <div style={{ backgroundColor: "green" }}>hi</div>
+        <div style={{ backgroundColor: "yellow" }}>hi</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
